39edb5bcfb945c3e6e2802a6d03a3c03b9490531
itmoti <motiarrahman9707@gmail.com>
2022-11-08 20:41:34 +0600

    one service find added

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const cors = require('cors');
 const port =  process.env.PORT || 5000
 const data = require('./data.json')
-const { MongoClient, ServerApiVersion, UnorderedBulkOperation } = require('mongodb');
+const { MongoClient, ServerApiVersion, UnorderedBulkOperation, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middlewire 
 app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.rat3bcl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -29,7 +30,16 @@ async function run() {
      const services = await cursor.limit(3).toArray()
       res.send(services) 
 
-  }) }
+  }) 
+ 
+  app.get('/service/:id' , async(req , res ) => {
+    const id = req.params.id;
+    console.log(id)
+   const query = {_id : ObjectId(id)}
+    const result = await database.findOne(query)
+    console.log(result)
+  })
+}
   catch{
     console.error(err)
   }
