ad35e0c242afaca2736f71020e173092db18787e
itmoti <motiarrahman9707@gmail.com>
2022-11-08 19:32:58 +0600

    services sectin finished

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ const app = express()
 const cors = require('cors');
 const port =  process.env.PORT || 5000
 const data = require('./data.json')
-import { MongoClient } from "mongodb";
+const { MongoClient, ServerApiVersion, UnorderedBulkOperation } = require('mongodb');
+require('dotenv').config()
+
 // middlewire 
 app.use(cors())
 
-const uri = 
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.rat3bcl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/services' , (req , res) => {
-    res.send(data)
-})
+async function run() {
+  try{
+    const database = client.db('kowshikPhotography').collection('services')
+    app.get('/services' , async(req , res) => {
+    const query = {}
+     const cursor = database.find(query)
+     const services = await cursor.toArray()
+      res.send(services)
+   
+  })
+
+  app.get('/home/services' , async(req , res) => {
+    const query = {}
+     const cursor = database.find(query)
+     const services = await cursor.limit(3).toArray()
+      res.send(services) 
+
+  }) }
+  catch{
+    console.error(err)
+  }
+}
+run().catch(err => console.log(err))
+
 
 
 app.get('/', (req, res) => {
