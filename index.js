9a5ab8de13c03c5406e5d7855710fc87dff2eb2d
itmoti <motiarrahman9707@gmail.com>
2022-11-09 21:46:26 +0600

    add review backend side added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port =  process.env.PORT || 5000
-const data = require('./data.json')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, UnorderedBulkOperation, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -16,6 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try{
     const database = client.db('kowshikPhotography').collection('services')
+    const serviceReview = client.db('kowshikPhotography').collection('review')
     app.get('/services' , async(req , res) => {
     const query = {}
      const cursor = database.find(query)
@@ -34,11 +35,30 @@ async function run() {
  
   app.get('/service/:id' , async(req , res ) => {
     const id = req.params.id;
-    console.log(id)
+   
    const query = {_id : ObjectId(id)}
     const result = await database.findOne(query)
     res.send(result)
   })
+
+
+  app.post('/service/:id' , async(req , res) => {
+    const id = req.params.id
+    const info = req.body
+    console.log(info)
+   
+    const result = await serviceReview.insertOne(info)
+    
+    res.send(result)
+  })
+
+  app.post('/jwt', (req,res) => {
+    const user = req.body;
+ 
+    var token = jwt.sign(user , process.env.SECRET_KEY)
+    res.send({token})
+    console.log({token})
+})
 }
   catch{
     console.error(err)
